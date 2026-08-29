2448f77d16b3099cf9a580668c2c633a1968b49e
Daniela Meneses <lumc446@gmail.com>
2026-08-29 17:30:39 -0500

    Modulo de usuarios

---
+++ usuarios.py
@@ -0,0 +1,10 @@
+usuarios = [
+    {"nombre": "Deybi", "area": "Sistemas"},
+    {"nombre": "Carlos", "area": "RRHH"},
+    {"nombre": "Ana", "area": "Contabilidad"}
+]
+
+print("Lista de usuarios:")
+
+for usuario in usuarios:
+    print(usuario["nombre"], "-", usuario["area"])
